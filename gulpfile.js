ae291ed7db67e1808a2b8085aa36b27d0db8d94e
Muskzogateberg <albert.zohery.rakoto@gmail.com>
2021-03-22 12:39:41 +0300

    Browser-sync fixed using gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,71 @@
-const gulp = require('gulp');
-const jshint = require('gulp-jshint');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const runSequence = require('gulp4-run-sequence');
-const browserSync = require('browser-sync').create();
+// Initialize modules
+const gulp = require('gulp'); // gulp4 { src, dest, watch, series, parallel}
+const jshint = require('gulp-jshint'); // verify code quality
+const babel = require('gulp-babel'); // transpile code
+const uglify = require('gulp-uglify'); // minimify code
+const runSequence = require('gulp4-run-sequence'); // use it in default task
+const { parallel } = require('gulp');
+const browserSync = require('browser-sync').create(); // use it to reload browser after changes
 
+// File path variables
+const files = {
+    htmlPath: './*.html',
+    jsPath: './*.js',
+    imgPath: './images/*',
+    babelPolyfill: './node_modules/babel-polyfill/browser.js'
+};
 
-gulp.task('default', (callback) => {
-    runSequence(gulp.parallel('processHTML', 'processJS', 'babelPolyfill', 'processIMG'), 'watch', callback());
-});
+/* ---- Tasks start here ---- */
 
-gulp.task('processHTML', () => {
-    return gulp.src('*.html')
+// processHTML
+function processHTML (){
+    return gulp.src(files.htmlPath)
     .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('processJS', () => {
-    return gulp.src('*.js')
+// processJS
+function processJS (){
+    return gulp.src(files.jsPath)
     .pipe(jshint( {esversion: 8} ))
     .pipe(jshint.reporter('default'))
     .pipe(babel({ presets: ['@babel/preset-env']}))
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('babelPolyfill', () => {
-    return gulp.src('node_modules/babel-polyfill/browser.js')
-    .pipe(gulp.dest('dist/node_modules/babel-polyfill'));
-});
+// babelPolyfill task
+function babelPolyfill (){
+    return gulp.src(files.babelPolyfill)
+    .pipe(gulp.dest('dist/node_modules/babel-polyfill/'));
+}
 
-gulp.task('processIMG', () => {
-    return gulp.src('./images/*.svg')
-    .pipe(gulp.dest('dist/images'));
-});
+// processIMG
+function processIMG (){
+    return gulp.src(files.imgPath)
+    .pipe(gulp.dest('dist/images/'));
+}
 
-gulp.task('browserSync', () => {
-    browserSync.init({
+/* ---- Tasks end here ---- */
+
+// Watch
+function watch(){
+    browserSync.init({ 
         server: {
             baseDir: './dist/'
         }
     });
-});
-
-function reload(done) {
-    browserSync.reload();
-    done();
+    gulp.watch(files.htmlPath, processHTML);
+    gulp.watch(files.jsPath, processJS);
+    gulp.watch(files.babelPolyfill, babelPolyfill);
+    gulp.watch(files.imgPath, processIMG);
+    gulp.watch('dist/*.html').on('change', browserSync.reload);
+    gulp.watch('dist/*.js').on('change', browserSync.reload);
+    gulp.watch('dist/images/*').on('add', browserSync.reload);
 }
 
-gulp.task('watch', gulp.series('browserSync'), () => {
-    gulp.watch('*.js', 'processJS');
-    gulp.watch('*.html', 'processHTML');
-    gulp.watch('images/*', 'processIMG');
-    gulp.watch('./dist/*.js').on('change', gulp.series(reload));
-    gulp.watch('./dist/*.html').on('change', gulp.series(reload));
-    gulp.watch('./dist/images/*').on('change', gulp.series(reload));
-});
+exports.watch = watch;
+
+exports.default = gulp.series (
+    gulp.parallel(processHTML, processJS, processIMG, babelPolyfill),
+    watch
+);
